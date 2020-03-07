6b7d3df2068f42cbc6f37816e8a81517aa89eaec
IamZY <562018301@qq.com>
2020-03-07 10:28:01 +0800

    offer

---
+++ src/com/offer/Q18/Solution.java
@@ -0,0 +1,27 @@
+package com.offer.Q18;
+
+/**
+ * 将一个字符串转换成一个整数，要求不能使用字符串转换整数的库函数。 数值为0或者字符串不是一个合法的数值则返回0
+ *
+ * @Author IamZY
+ * @create 2020/2/24 19:42
+ */
+public class Solution {
+
+    public int StrToInt(String str) {
+        char[] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+
+
+
+
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
