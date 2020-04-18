4a344ece2206b41c46d060f9d13a62a9da95f7b2
IamZY <562018301@qq.com>
2020-04-18 21:44:09 +0800

    实现 strStr()

---
+++ src/com/offer/Q23/Solution.java
@@ -0,0 +1,46 @@
+package com.offer.Q23;
+
+/**
+ * 实现 strStr() 函数。
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ *
+ * @Author IamZY
+ * @create 2020/4/18 10:52
+ */
+public class Solution {
+
+    public int strStr(String haystack, String needle) {
+//        return haystack.indexOf(needle);
+
+        int L = needle.length(), n = haystack.length();
+        if (L == 0) return 0;
+
+        int pn = 0;
+        while (pn < n - L + 1) {
+            // find the position of the first needle character
+            // in the haystack string
+            while (pn < n - L + 1 && haystack.charAt(pn) != needle.charAt(0)) ++pn;
+
+            // compute the max match string
+            int currLen = 0, pL = 0;
+            while (pL < L && pn < n && haystack.charAt(pn) == needle.charAt(pL)) {
+                ++pn;
+                ++pL;
+                ++currLen;
+            }
+
+            // if the whole needle string is found,
+            // return its start position
+            if (currLen == L) return pn - L;
+
+            // otherwise, backtrack
+            pn = pn - currLen + 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().strStr("hello", "ll"));
+    }
+
+}
