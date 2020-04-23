06f7f20a0d987033bfc8c8b770ef7f015a948741
IamZY <562018301@qq.com>
2020-04-23 15:31:37 +0800

    x 的平方根

---
+++ src/com/offer/Q26/Solution.java
@@ -0,0 +1,30 @@
+package com.offer.Q26;
+
+/**
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ *
+ * @Author IamZY
+ * @create 2020/4/23 14:21
+ */
+public class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long num;
+        int pivot, left = 2, right = x / 2;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            num = (long) pivot * pivot;
+            if (num > x) right = pivot - 1;
+            else if (num < x) left = pivot + 1;
+            else return pivot;
+        }
+
+        return right;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().mySqrt(10));
+    }
+}
