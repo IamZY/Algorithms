5c05724ed8fdabdf685355daffa8db37427a8cfd
IamZY <562018301@qq.com>
2020-05-06 16:07:19 +0800

    二叉树的最大深度

---
+++ src/com/offer/Q31/Solution.java
@@ -0,0 +1,59 @@
+package com.offer.Q31;
+
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * @Author IamZY
+ * @create 2020/5/6 15:40
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    // 深度优先遍历
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left,right) + 1;
+        }
+    }
+
+    // 广度优先遍历
+    public int maxDepth2(TreeNode root) {
+        Queue<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+        if (root != null) {
+            stack.add(new Pair(root, 1));
+        }
+
+        int depth = 0;
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> current = stack.poll();
+            root = current.getKey();
+            int current_depth = current.getValue();
+            if (root != null) {
+                depth = Math.max(depth, current_depth);
+                stack.add(new Pair(root.left, current_depth + 1));
+                stack.add(new Pair(root.right, current_depth + 1));
+            }
+        }
+        return depth;
+    }
+    public static void main(String[] args){
+
+    }
+}
